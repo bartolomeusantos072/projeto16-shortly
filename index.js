30ba2dd10622404b093f6fa717cd0d56a62d97f7
Uenderss <meajudabarto@gmail.com>
2022-08-05 08:39:53 -0300

    estruturacao das pastas do app

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import router from './routes/index.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app=express();
 
 app.use(express.json());
+app.use(cors());
+app.use(router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
