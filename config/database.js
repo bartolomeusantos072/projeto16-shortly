151cfe6ae4fcc5d8b2c4ba17d26adc65a99ddeaa
Uenderss <meajudabarto@gmail.com>
2022-08-08 17:06:12 -0300

    Prepare for deploy

--- config/database.js
+++ config/database.js
@@ -5,7 +5,10 @@ dotenv.config();
 const { Pool } = pg;
 const configDatabase = {
   connectionString: process.env.DATABASE_URL
-  .ssl = {
+};
+
+if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
     rejectUnauthorized: false
   }
 }
